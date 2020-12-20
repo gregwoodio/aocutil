dd64bcd4a174ca6ac5b01dd963de862e3a7c098b
Greg Wood <contact@gregwood.io>
2020-12-20 18:57:55 -0500

    Fix UT

--- geometry_test.go
+++ geometry_test.go
@@ -6,7 +6,7 @@ func TestGetSlope(t *testing.T) {
 	a := Coord{5, 9}
 	b := Coord{13, 11}
 
-	dx, dy := a.GetSlope(&b)
+	dx, dy := b.GetSlope(&a)
 	if dx != 4 && dy != 1 {
 		t.Errorf("Expected delta X to be %d and delta Y to be %d, but were %d and %d\n", 4, 1, dx, dy)
 	}
